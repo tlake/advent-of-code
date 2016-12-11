892ac9dee30ee51588c752cebcc2eb6539a4d815
Tanner Lake <tanner.lake@gmail.com>
2016-12-10 21:22:00 -0800

    Day 5, Part 2

--- 2016/day05_how_about_a_nice_game_of_chess/python/src/test_solutions.py
+++ 2016/day05_how_about_a_nice_game_of_chess/python/src/test_solutions.py
@@ -2,7 +2,7 @@
 import pytest
 
 from part1 import PasswordFinder as PasswordFinder1
-# from part2 import PasswordFinder as PasswordFinder2
+from part2 import PasswordFinder as PasswordFinder2
 
 
 class TestPart1UnitTests:
@@ -17,12 +17,13 @@ class TestPart1UnitTests:
         assert(finder.find_password(test_input) == expected)
 
 
-# class TestPart2UnitTests:
-#     """."""
+class TestPart2UnitTests:
+    """."""
 
-#     @pytest.mark.parametrize("test_input, expected", [
-#         ('input', 'expect'),
-#     ])
-#     def test_something(self, test_input, expected):
-#         """."""
-#         pass
+    @pytest.mark.parametrize("test_input, expected", [
+        ('abc', '05ace8e3'),
+    ])
+    def test_find_password(self, test_input, expected):
+        """."""
+        finder = PasswordFinder2()
+        assert(finder.find_password(test_input) == expected)
