2d165d121c7b5f7529e34ca17c667821d736aa2e
Tanner Lake <tanner.lake@gmail.com>
2016-12-10 18:21:59 -0800

    Day 4, Part 2

--- 2016/day04_security_through_obscurity/python/src/solution2.py
+++ 2016/day04_security_through_obscurity/python/src/solution2.py
@@ -1,15 +1,59 @@
 #!/usr/bin/env python
 """Docstring."""
 
-from common import (
-    get_input,
-)
+from collections import Counter
+
+from common import get_input
 
 
-def main():
-    """Docstring."""
-    puzzle_input = get_input()
+class RoomAnalyzer:
+    """."""
+
+    def __init__(self, input_list=[]):
+        """."""
+        self.input_list = input_list
+
+    def process_room_string(self, room_string):
+        """."""
+        name = room_string[:-11].replace("-", " ")
+        sector_id = int(room_string[-10:-7])
+        checksum = room_string[-6:-1]
+        return name, sector_id, checksum
+
+    def room_is_real(self, room_name, room_checksum):
+        """."""
+        counter = Counter(room_name.replace(" ", ""))
+        checked = ''.join(map(lambda x: x[0], sorted(counter.most_common(), key=lambda x: (-x[1], x[0]))))[:5]
+        return bool(checked == room_checksum)
+
+    def _shift(self, char_and_shift):
+        """."""
+        char, amount = char_and_shift[0], char_and_shift[1] % 26
+        if char == ' ':
+            return char
+
+        o = ord(char) + amount
+        new = o if o < 123 else o - 26
+
+        return chr(new)
+
+    def decipher(self, room_name, shift):
+        """."""
+        room_name = room_name.lower()
+        shift = shift % 26
+        return ''.join(map(self._shift, [(x, shift) for x in room_name]))
+
+    def analyze_room(self, room_string):
+        """."""
+        name, sector_id, checksum = self.process_room_string(room_string)
+        return (self.decipher(name, sector_id), sector_id) if self.room_is_real(name, checksum) else None
+
+    def analyze_input(self):
+        """."""
+        return list(filter(lambda x: x is not None and all([k in x[0] for k in ["north", "pole", "object", "storage"]]), map(self.analyze_room, self.input_list)))
 
 
 if __name__ == "__main__":
-    main()
+    puzzle_input = get_input()
+    analyzer = RoomAnalyzer(get_input())
+    print(analyzer.analyze_input())
--- 2016/day04_security_through_obscurity/python/src/test_solutions.py
+++ 2016/day04_security_through_obscurity/python/src/test_solutions.py
@@ -1,9 +1,11 @@
 """."""
 import pytest
-from solution1 import RoomAnalyzer
+
+from solution1 import RoomAnalyzer as ra1
+from solution2 import RoomAnalyzer as ra2
 
 
-class TestSolution1:
+class TestPart1UnitTests:
     """."""
 
     @pytest.mark.parametrize("test_input, expected", [
@@ -14,7 +16,7 @@ class TestSolution1:
     ])
     def test_process_room_string(self, test_input, expected):
         """."""
-        analyzer = RoomAnalyzer()
+        analyzer = ra1()
         assert(analyzer.process_room_string(test_input) == expected)
 
     @pytest.mark.parametrize("test_input, expected", [
@@ -25,7 +27,7 @@ class TestSolution1:
     ])
     def test_room_is_real(self, test_input, expected):
         """."""
-        analyzer = RoomAnalyzer()
+        analyzer = ra1()
         name, _, checksum = analyzer.process_room_string(test_input)
         assert(analyzer.room_is_real(name, checksum) == expected)
 
@@ -37,7 +39,7 @@ class TestSolution1:
     ])
     def test_analyze_room(self, test_input, expected):
         """."""
-        analyzer = RoomAnalyzer()
+        analyzer = ra1()
         assert(analyzer.analyze_room(test_input) == expected)
 
     def test_analyze_input(self):
@@ -49,6 +51,29 @@ class TestSolution1:
             "totally-real-room-200[decoy]",
         ]
         expected = 1514
-        """."""
-        analyzer = RoomAnalyzer(test_input)
+        analyzer = ra1(test_input)
         assert(analyzer.analyze_input() == expected)
+
+
+class TestPart2UnitTests:
+    """."""
+
+    @pytest.mark.parametrize("test_input, expected", [
+        (('a', 2), 'c'),
+        (('a', 25), 'z'),
+        (('a', 26), 'a'),
+        (('z', 2), 'b'),
+        (('a', 53), 'b'),
+        ((' ', 2), ' ')
+    ])
+    def test__shift(self, test_input, expected):
+        """."""
+        analyzer = ra2()
+        assert(analyzer._shift(test_input) == expected)
+
+    def test_decipher(self):
+        """."""
+        test_input = ("qzmt zixmtkozy ivhz", 343)
+        expected = "very encrypted name"
+        analyzer = ra2()
+        assert(analyzer.decipher(test_input[0], test_input[1]) == expected)
